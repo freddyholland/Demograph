b41f9fd6c5163fb224d21c98ab93d64af572c602
freddyholland <f.holland124@hotmail.com>
2020-04-28 22:46:05 +1000

    Added Spotify/Twitch APIs & started changing storage system.

---
+++ Demograph/Classes/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  Demograph
+//
+//  Created by iMac on 28/4/20.
+//  Copyright © 2020 Frederick Holland. All rights reserved.
+//
+
+import Foundation
---
+++ Demograph/Controllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Demograph
+//
+//  Created by iMac on 27/4/20.
+//  Copyright © 2020 Frederick Holland. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
